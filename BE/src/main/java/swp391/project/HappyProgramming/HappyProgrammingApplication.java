8fdc01428da99da79737a1fdd97b911ffb01e2f3
huyvo <huyvvphe186830@fpt.edu.vn>
2024-09-16 23:02:15 +0700

    Update base

--- BE/src/main/java/swp391/project/HappyProgramming/HappyProgrammingApplication.java
+++ BE/src/main/java/swp391/project/HappyProgramming/HappyProgrammingApplication.java
@@ -3,10 +3,13 @@ package swp391.project.HappyProgramming;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
 public class HappyProgrammingApplication {
 
 	public static void main(String[] args) {
+		System.out.println("hehhe");
 		SpringApplication.run(HappyProgrammingApplication.class, args);
 	}
 
